09275aae0073546b93b16bbb0ceff067ef8e171c
Wonakee Amos <mulamos@Wonakees-MacBook.local>
2016-11-09 11:23:07 -0500

    Question 4

--- main.js
+++ main.js
@@ -1,13 +1,40 @@
 $(document).ready(function(){
-	$("#search").click(function(){
-		var query = $(#query).val();
-		var url = "https://info2180-lab6-mulamos.c9users.io/request.php?q="+query;
-		$.ajax(url, {
-			method: 'GET',
-		}).done(function(result){
-			$("#result").html(result);
-		}).fail(function(error){
-			$("#result").html(error);
-		});
-	});	
+    
+    //search button entered text
+    $("#search").on('click', function(){
+       var query = $("#query").val();
+       var link = "https://info2180-lab6-mulamos.c9users.io/request.php?q="+query;
+       
+       $.ajax(link, {
+           method: 'GET',
+       }).done(function(res){
+           $("#result").html(res);
+       }).fail(function(err){
+           $("#result").html(err);
+       });
+    });
+    
+    //search all button get all definition
+    $("#alldefs").on('click', function(){
+        var link = "https://info2180-lab6-mulamos.c9users.io/request.php?q=&all=true";
+        
+        $.ajax(link, {
+           method: 'GET',
+           dataType: "xml",
+       }).done(function(res){
+           var defs = $(res).find('definition');
+          
+           $("#result").append('<ol></ol>');
+           
+           $(defs).each(function(){
+               var ret = '<h3>'+$(this).attr('name')+'</h3>';
+               ret += '<p>'+$(this).text()+'</p>';
+               ret += '<p> -'+$(this).attr('author')+'</p>';
+               $("#result ol").append('<li>'+ret+'</li>');
+           });
+       }).fail(function(err){
+           $("#result").html(err);
+       });
+    });
+    
 });
--- request.php
+++ request.php
@@ -11,6 +11,36 @@ $definition = [
     "javascript" => "A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.",
     "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
 ];
-
-print "<h3>" . strtoupper($query) . "</h3>";
-print "<p>" . $definition[$query] . "</p>";
+$xmldata = '<?xml version="1.0" encoding="UTF-8"?>
+<entries> 
+    <definition name="Definition" author="John"> 
+        A statement of the exact meaning of a word, especially in a dictionary. 
+    </definition> 
+    <definition name="Bar" author="Mary"> 
+        A place that sells alcholic beverages.
+    </definition> 
+    <definition name="AJAX" author="Kimberly"> 
+        Technique which involves the use of javascript and xml (or JSON).
+    </definition> 
+    <definition name="HTML" author="Anthony">
+        The standard markup language for creating web pages and web applications.
+    </definition>
+    <definition name="CSS" author="Khalid">
+        A style sheet language used for describing the presentation of a document written in a markup language.
+    </definition>
+    <definition name="JavaScript" author="Nick">
+        A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.
+    </definition>
+    <definition name="PHP" author="Oraine">
+        A server-side scripting language, and a powerful tool for making dynamic and interactive websites.
+    </definition>
+</entries>';
+if ($query == ""){
+    header('Content-Type: text/xml');
+    $xmlOutput = new SimpleXMLElement($xmldata);
+    echo $xmlOutput->asXML();
+}
+else{
+    print "<h3>" . strtoupper($query) . "</h3>";
+    print "<p>" . $definition[$query] . "</p>";
+}
